653a647422deb0d7bfa9b42c94c51dd8db247577
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-12 16:50:51 -0600

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+   title = 'com-next-tech-admin-front-analytics-2';
+}
