6facbd5639dff763c3406c493ad0cea6084a5c06
luis.villar <luis.villar@next-technologies.com.mx>
2023-07-03 15:12:53 -0600

    rutas para facturas emisión agregadas

--- src/app/modules/merge/merge-routing.module.ts
+++ src/app/modules/merge/merge-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableConciliadasComponent } from './pages/facturas-emision/facturas-conciliadas/table-conciliadas/table-conciliadas.component';
+import { TableFaltantesComponent } from './pages/facturas-emision/facturas-faltantes-sat/table-faltantes/table-faltantes.component';
+import { TableDescuadreComponent } from './pages/facturas-emision/facturas-fecha-descuadre/table-descuadre/table-descuadre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'fac_emi_conciliadas', component: TableConciliadasComponent },
+  {path: 'fac_emi_fal_sat'    , component: TableFaltantesComponent   },
+  {path: 'fac_emi_descuadre'  , component: TableDescuadreComponent   },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
