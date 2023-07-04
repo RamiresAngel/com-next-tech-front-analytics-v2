9ecb550d03d8042d3b5c233163f1b9ebfea30e2b
luis.villar <luis.villar@next-technologies.com.mx>
2023-07-04 14:11:15 -0600

    reactive forms api generar hoteles en select/option

--- src/app/modules/merge/pages/facturas-emision/facturas-conciliadas/form-conciliadas/form-conciliadas.component.ts
+++ src/app/modules/merge/pages/facturas-emision/facturas-conciliadas/form-conciliadas/form-conciliadas.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CatalogoNiveles, UserData } from 'src/app/shared/entities';
+import { ConfiguracionesService } from 'src/app/shared/services/configuraciones.service';
 
 @Component({
   selector: 'app-form-conciliadas',
@@ -9,8 +11,26 @@ import { FormGroup } from '@angular/forms';
 export class FormConciliadasComponent {
   public visible: boolean = false;
   public formularioConciliadas!: FormGroup;
+  public hoteles!: CatalogoNiveles[];
+  public dataUser!: UserData;
+  public userDataStorage: any = localStorage.getItem('dataUser');
 
-  constructor() {};
+  constructor(
+    private configuracionesService : ConfiguracionesService
+  ) {
+    this.dataUser = JSON.parse(this.userDataStorage);
+    this.hoteles = this.dataUser.catalogo_niveles;
+
+    this.formularioConciliadas = new FormGroup({
+      hotelControl: new FormControl(this.hoteles[0], Validators.required),
+    });
+
+  };
+
+  public obtenerRfcEmisores(event: any): any {
+    console.log('GENERAR RFC EMISORES');
+
+  };
 
   public limpiarFiltros(): void {
     console.log('limpiar filtros');
@@ -18,7 +38,8 @@ export class FormConciliadasComponent {
 
   public enviarDatosFiltro(): void {
     console.log('enviar datos filtro');
-    this.visible = false;
+    console.log('formulario', this.formularioConciliadas.value);
+    // this.visible = false;
   };
 
   public open(): void {
--- src/environments/endpoints.ts
+++ src/environments/endpoints.ts
@@ -1,6 +1,10 @@
 export const globalApis = {
    url_vault: 'https://com-next-tech-tax-vault-develop-mb2bvo7jba-uc.a.run.app',
    url_proveedores: 'https://analytics-proveedores-lista-develop-mb2bvo7jba-uc.a.run.app',
-   url_usuarios: 'https://analytics-configuracion-usuarios-develop-auth0-mb2bvo7jba-uc.a.run.app',
+   url_usuarios: 'https://analytics-configuracion-usuarios-develop-mb2bvo7jba-uc.a.run.app',
    url_contribuyentes: 'https://analytics-configuracion-contribuyentes-develop-mb2bvo7jba-uc.a.run.app'
 }
+
+// No me digas eso!!, este comentario será borrado gracias Ángel !!
+// this.url_usuarios =  'https://analytics-configuracion-usuarios-develop-mb2bvo7jba-uc.a.run.app';
+// url_usuarios: 'https://analytics-configuracion-usuarios-develop-auth0-mb2bvo7jba-uc.a.run.app'
