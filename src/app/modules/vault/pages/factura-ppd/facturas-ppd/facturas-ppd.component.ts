eabb01fbadc940dabcb4383e08b68cae20cd178d
Angel Ramirez <angel.ramirez@next-technologies.com.mx>
2023-07-03 18:21:14 -0600

    🔧 fix(facturas-ppd.component.html): actualizar componente de filtro para refrescar la tabla ✨ feat(facturas-ppd.component.html): añadir soporte para mostrar un loader mientras se carga la tabla 🔧 fix(facturas-ppd.component.ts): actualizar importaciones y variables para el componente de facturas PPD ✨ feat(facturas-ppd.component.ts): añadir lógica para inicializar y refrescar la tabla de facturas PPD
    🔧 fix(ppd-filtro.component.html): corregir errores de sintaxis y nombres de funciones
    🔧 fix(ppd-filtro.component.html): corregir errores de sintaxis y nombres de funciones
    
    🔧 fix(ppd-filtro.component.ts): cambiar import de UserData y BodyFiltro
    🔧 fix(ppd-filtro.component.ts): añadir validación requerida al campo nivel_acceso
    🔧 fix(ppd-filtro.component.ts): cambiar nombre de variable rango_estatus a rango_estatus_f
    🔧 fix(ppd-filtro.component.ts): cambiar nombre de función selectedVigencia a selectedCatalogoNivel
    🔧 fix(ppd-filtro.component.ts): cambiar nombre de función getTipoFactura a getEfectoComprobante
    🔧 fix(ppd-filtro.component.ts): cambiar nombre de función onChange a onChangeFechaFactura
    🔧 fix(ppd-filtro.component.ts): cambiar nombre de función rangoCancelacion a onChangeRangoCancelacion
    🔧 fix(ppd-filtro.component.ts): cambiar nombre de función generarReporte a generaReporte
    ✨ feat(ppd-filtro.component.ts): añadir soporte para emitir evento de filtro

--- src/app/modules/vault/pages/factura-ppd/facturas-ppd/facturas-ppd.component.ts
+++ src/app/modules/vault/pages/factura-ppd/facturas-ppd/facturas-ppd.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { UserData } from 'src/app/shared/entities/userData.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { BodyFiltro, ColumnsHeader, UserData } from 'src/app/shared/entities';
+import { globalApis } from 'src/environments/endpoints';
 
 @Component({
   selector: 'app-facturas-ppd',
@@ -7,11 +9,112 @@ import { UserData } from 'src/app/shared/entities/userData.model';
   styleUrls: ['./facturas-ppd.component.scss']
 })
 export class FacturasPPDComponent {
+  @ViewChild('facturas') facturas!: HTMLElement;
+  public dtOptions: DataTables.Settings = {};
+  public dataTablesParameters: any;
+  public filtro = new BodyFiltro();
+  public columns_header = new ColumnsHeader();
   public dataUserStorage: any = localStorage.getItem("dataUser");
   public dataUser!: UserData;
+  public is_loading: boolean = false;
+  public bodyGlobalFiltro: any = {
+    filtro: {}
+  };
 
-  constructor() {
+  constructor(
+    private _http: HttpClient
+  ) {
     this.dataUser = JSON.parse(this.dataUserStorage)
+  }
+
+  ngOnInit(): void {
+    this.inicializaTabla();
+  }
+
+  inicializaTabla() {
+    this.dtOptions = {
+      serverSide: true,
+      processing: true,
+      scrollX: true,
+      searching: false,
+      columnDefs: [
+        {
+          targets: '_all',
+          className: 'text-center'
+        }
+      ],
+      ajax: (dataTablesParameters: any, callback) => {
+        const aux_filtro = this.meterFiltro(dataTablesParameters);
+        if (aux_filtro.filtro.rfc_emisor != '') {
+          dataTablesParameters = aux_filtro;
+          console.log(dataTablesParameters);
+          this._http.post<any>(
+            `${globalApis.url_vault}/factura`,
+            dataTablesParameters, {
+            headers: new HttpHeaders({
+              'Authorization': `Bearer ${localStorage.getItem('token')}`,
+              'Content-Type': 'application/json'
+            })
+          },
+          ).subscribe((resp: any) => {
+            callback({
+              recordsTotal: resp.data.total,
+              recordsFiltered: resp.data.total,
+              data: resp.data.data
+            });
+          }, (error) => {
+            console.log(error);
+            callback({
+              recordsTotal: 0,
+              recordsFiltered: 0,
+              data: []
+            });
+          });
+        } else {
+          callback({
+            recordsTotal: 0,
+            recordsFiltered: 0,
+            data: []
+          });
+        }
+      },
+      columns: []
+    }
+  }
+
+  meterFiltro(obj: any = null) {
+    const body_filtro = this.filtro;
+    if (body_filtro) {
+      obj = {};
+      obj = body_filtro;
+      this.filtro = body_filtro;
+    }
+    if (obj) {
+      this.dataTablesParameters = obj;
+    } else {
+      obj = this.dataTablesParameters;
+    }
+    obj = this.filtro;
+    obj.columns = [{
+      dir: 'asc'
+    }];
+    return obj;
+  }
+
+  public refrescaTabla(filtro: BodyFiltro) {
+    if (filtro.filtro.rfc_emisor == '' || filtro.filtro.rfc_emisor == null || filtro.filtro.rfc_emisor == undefined
+      || filtro.filtro.fecha_factura_i == '' || filtro.filtro.fecha_factura_i == null || filtro.filtro.fecha_factura_i == undefined) {
+      alert('El campo RFC Emisor es requerido');
+    } else {
+      if (filtro) {
+        this.filtro = filtro;
+      }
+      try {
+        $('#analytics-table').DataTable().ajax.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
 }
--- src/app/modules/vault/pages/factura-ppd/ppd-filtro/ppd-filtro.component.ts
+++ src/app/modules/vault/pages/factura-ppd/ppd-filtro/ppd-filtro.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { UserData } from 'src/app/shared/entities/userData.model';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UtilsService } from 'src/app/shared/services/utils.service';
 import { GeneraReporteComponent } from '../../../Shared/genera-reporte/genera-reporte.component';
+import { BodyFiltro, RFCMap, RfcHotelUser, UserData } from 'src/app/shared/entities';
+import { VaultService } from 'src/app/shared/services/vault.service';
 
 @Component({
   selector: 'app-ppd-filtro',
@@ -11,21 +12,25 @@ import { GeneraReporteComponent } from '../../../Shared/genera-reporte/genera-re
 })
 export class PPDFiltroComponent {
   @ViewChild('modal') modal?: GeneraReporteComponent;
+  @Output() filtro = new EventEmitter<BodyFiltro>();
   public visible = false;
-  public nivelAccesoSelected: any;
   public dataUser!: UserData;
-  public formFilters: FormGroup = new FormGroup({});
+  public nivelAccesoSelected: any;
+  public rfc_map: RFCMap[] = [];
+  public serie_hotel: Array<string> = [];
+  public option_serie_hotel: string[] = []
   public tipo_factura: string = 'Emision';
   public efecto_comprobante: string = '';
-  public rango_estatus: string = 'Vigentes/Canceladas';
   public date_factura = null;
   public date_rango = null;
-  public dateFormat = 'dd/MM/yyyy';
   public vista_Fecha: boolean = true;
   public vista_Rango: boolean = true;
+  public bodyFiltro: BodyFiltro = new BodyFiltro();
+  public formFilters: FormGroup = new FormGroup({});
 
   constructor(
     private utils_service: UtilsService,
+    private vault_service: VaultService
   ) {
     this.dataUser = JSON.parse(localStorage.getItem("dataUser")!);
     this.iniciaFormFiltro();
@@ -33,10 +38,10 @@ export class PPDFiltroComponent {
 
   iniciaFormFiltro(): void {
     this.formFilters = new FormGroup({
-      nivel_acceso: new FormControl(this.nivelAccesoSelected),
+      nivel_acceso: new FormControl(this.nivelAccesoSelected, [Validators.required]),
       rfc_emisor: new FormControl(''),
       rfc_receptor: new FormControl(''),
-      tipo_factura: new FormControl('Emision'),
+      tipo_factura: new FormControl(),
       serie_hotel: new FormControl([]),
       serie: new FormControl(''),
       folio: new FormControl(''),
@@ -44,40 +49,77 @@ export class PPDFiltroComponent {
       rfc_pac: new FormControl(''),
       efecto_comprobante: new FormControl(''),
       uuid: new FormControl(''),
-      rango_estatus: new FormControl('Vigentes/Canceladas'),
-      fecha_factura: new FormControl(),
+      rango_estatus: new FormControl(''),
+      fecha_factura: new FormControl([Validators.required]),
       rango_cancelacion: new FormControl(''),
     });
   }
 
   filtrar(): void {
-    console.log(this.formFilters.value);
+    this.bodyFiltro.filtro.rfc_emisor = this.formFilters.value.rfc_emisor;
+    this.bodyFiltro.filtro.rfc_receptor = this.formFilters.value.rfc_receptor;
+    this.bodyFiltro.filtro.fecha_factura_i = this.formFilters.value.fecha_factura.inicio;
+    this.bodyFiltro.filtro.fecha_factura_f = this.formFilters.value.fecha_factura.fin;
+    this.bodyFiltro.filtro.serie_hotel = this.formFilters.value.serie_hotel;
+    this.bodyFiltro.filtro.serie = this.formFilters.value.serie;
+    this.bodyFiltro.filtro.folio = this.formFilters.value.folio;
+    this.bodyFiltro.filtro.cp = this.formFilters.value.codigo_postal;
+    this.bodyFiltro.filtro.rfc_pac = this.formFilters.value.rfc_pac;
+    this.bodyFiltro.filtro.efecto_comprobante = this.formFilters.value.efecto_comprobante;
+    this.bodyFiltro.filtro.folio_fiscal = this.formFilters.value.uuid;
+    this.bodyFiltro.filtro.estatus_factura = this.formFilters.value.rango_estatus_f;
+    this.bodyFiltro.filtro.fecha_cancelacion_i = this.formFilters.value.rango_cancelacion.inicio ? this.formFilters.value.rango_cancelacion.inicio : '';
+    this.bodyFiltro.filtro.fecha_cancelacion_f = this.formFilters.value.rango_cancelacion.fin ? this.formFilters.value.rango_cancelacion.fin : '';
+
+    this.bodyFiltro.tipo = this.formFilters.value.tipo_factura;
+    this.bodyFiltro.tipo_comprobante = 'factura';
+    this.bodyFiltro.ppd_view = false;
+    this.bodyFiltro.fidecomiso = false;
+    this.bodyFiltro.corporativo = this.dataUser.corporativo;
+    this.filtro.emit(this.bodyFiltro);
+  }
+
+  selectedCatalogoNivel(event: any): void {
+    this.nivelAccesoSelected = event;
+    this.getRFCEmisor(this.nivelAccesoSelected);
+  }
+
+  /* Función que consume api y obtiene un objeto para select RFC emisor */
+  getRFCEmisor(acceso: string): void {
+    let body = {
+      email: this.dataUser.email,
+      corporativo: this.dataUser.corporativo,
+      nivel_acceso: acceso,
+      rol: this.dataUser.rol
+    };
+    this.vault_service.getRFCMap(body).subscribe(
+      (data: RfcHotelUser) => {
+        this.rfc_map = data.rfc_map;
+      }, (error: any) => {
+        console.log(error);
+      });
+  }
+
+  selectedRfc(event: any, tipo: string): void {
+    this.option_serie_hotel = this.rfc_map.filter((item: RFCMap) => item.rfc === event)[0].serie;
   }
 
   selectedVigencia(event: any): void {
     let vista = this.utils_service.setFechasVista(event);
     this.vista_Fecha = vista.fecha;
     this.vista_Rango = vista.rango;
-    this.rango_estatus = event;
   }
 
-  getTipoFactura(event: any): void {
-    this.tipo_factura = event;
+  onChange(event: any) {
+    if (!event || event.length === 0) return;
+    const fecha = this.utils_service.obtenerFormatoFechas(event);
+    this.formFilters.patchValue({ fecha_factura: fecha });
   }
 
-  getEfectoComprobante(event: any): void {
-    console.log(event);
-    this.efecto_comprobante = event;
-  }
-
-  onChange(result: Date[]): void {
-    /* let fechas = this.utils_service.obtenerFormatoFechas(result);
-    this.formFilters.patchValue({ fecha_factura: fechas }); */
-  }
-
-  rangoCancelacion(result: Date[]): void {
-    /* let fechas = this.utils_service.obtenerFormatoFechas(result);
-    this.formFilters.patchValue({ rango_cancelacion: fechas }); */
+  rangoCancelacion(event: any) {
+    if (!event || event.length === 0) return;
+    const fecha = this.utils_service.obtenerFormatoFechas(event);
+    this.formFilters.patchValue({ rango_cancelacion: fecha });
   }
 
   open(): void {
@@ -86,15 +128,15 @@ export class PPDFiltroComponent {
 
   close(): void {
     this.visible = false;
+    this.iniciaFormFiltro();
   }
 
-  generarReporte(event: any): void {
+  generaReporte(event: any): void {
     console.log(event);
   }
 
   showModal() {
     this.modal?.showModal();
   }
-
 
 }
