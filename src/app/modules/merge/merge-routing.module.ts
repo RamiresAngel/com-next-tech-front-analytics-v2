af61415deee09c024421bc8adc872f2ab1567705
Angel Ramirez <angel.ramirez@next-technologies.com.mx>
2023-08-18 14:32:11 -0600

    feat: :art: Integración de las ruras y vista base del mudulo Pagos-Emisión
    codigo HTML para la vista de sol sigientes componentes Table-pagos(conciliados,descuadre y faltantes)

--- src/app/modules/merge/merge-routing.module.ts
+++ src/app/modules/merge/merge-routing.module.ts
@@ -9,6 +9,9 @@ import { TableConRecComponent } from './pages/facturas-recepcion/conciliadas-rec
 import { TableConciliadasValidComponent } from './pages/facturas-recepcion-valid/facturas-conciliadas-valid/table-conciliadas-valid/table-conciliadas-valid.component';
 import { TableDescuadreValidComponent } from './pages/facturas-recepcion-valid/facturas-descuadre-valid/table-descuadre-valid/table-descuadre-valid.component';
 import { TableFaltantesValidComponent } from './pages/facturas-recepcion-valid/facturas-faltantes-valid/table-faltantes-valid/table-faltantes-valid.component';
+import { TablePagosConciliadosComponent } from './pages/pagos-emision/pagos-conciliados/table-pagos-conciliados/table-pagos-conciliados.component';
+import { TablePagosFaltantesSatComponent } from './pages/pagos-emision/pagos-faltantes-sat/table-pagos-faltantes-sat/table-pagos-faltantes-sat.component';
+import { TablePagosDescuadreComponent } from './pages/pagos-emision/pagos-descuadre/table-pagos-descuadre/table-pagos-descuadre.component';
 
 const routes: Routes = [
   { path: 'fac_emi_conciliadas', component: TableConciliadasComponent },
@@ -19,7 +22,10 @@ const routes: Routes = [
   { path: 'fac_rec_descuadre', component: TableDesRecComponent },
   { path: 'fac_rec_valid_conciliadas', component: TableConciliadasValidComponent },
   { path: 'fac_rec_valid_fal_sat', component: TableFaltantesValidComponent },
-  { path: 'fac_rec_valid_descuadre', component: TableDescuadreValidComponent }
+  { path: 'fac_rec_valid_descuadre', component: TableDescuadreValidComponent },
+  { path: 'pag_emi_conciliadas', component: TablePagosConciliadosComponent },
+  { path: 'pag_emi_fal_sat', component: TablePagosFaltantesSatComponent },
+  { path: 'pag_emi_descuadre', component: TablePagosDescuadreComponent }
 ];
 
 @NgModule({
