b9265438e1548f33dacca4d22feac7f48f982dd3
Angel Ramirez <angel.ramirez@next-technologies.com.mx>
2023-08-17 20:37:07 -0600

    [add] vista de componentes del modulo Facturas recepción VALID

--- src/app/shared/entities/merge.model.ts
+++ src/app/shared/entities/merge.model.ts
@@ -207,7 +207,6 @@ export class ColumnsEmiDescuadre {
   }
 }
 
-
 export class ColumnsEmiFaltantes {
   columns_header: Array<{ title: string, data: string }>;
 
@@ -243,7 +242,6 @@ export class ColumnsEmiFaltantes {
     ];
   }
 }
-
 
 
 
@@ -417,6 +415,111 @@ export class ColumnsRecDescuadre {
       { title: 'FECHA FACTURA FACTO', data: '60' },
       { title: 'FECHA TIMBRADO FACTO', data: '61' },
       { title: 'DESCUADRE', data: '62' }
+    ];
+  }
+}
+
+
+
+export class ColumnsConciliadasValid {
+  columns_header: Array<{ title: string, data: string }>;
+
+  constructor() {
+    this.columns_header = [
+      { title: 'ESTATUS SAT', data: '0' },
+      { title: 'FECHA CANCELACIÓN SAT', data: '1' },
+      { title: 'FOLIO FISCAL SAT', data: '2' },
+      { title: 'RFC EMISOR SAT', data: '3' },
+      { title: 'RAZÓN SOCIAL EMISOR SAT', data: '4' },
+      { title: 'RFC RECEPTOR SAT', data: '5' },
+      { title: 'RAZÓN SOCIAL RECEPTOR SAT', data: '6' },
+      { title: 'RFC PAC SAT', data: '7' },
+      { title: 'FECHA EMISIÓN SAT (YYYY-MM-DD)', data: '8' },
+      { title: 'FECHA EMISIÓN SAT', data: '9' },
+      { title: 'FECHA TIMBRADO SAT', data: '10' },
+      { title: 'MONTO SAT', data: '11' },
+      { title: 'EFECTO COMPROBANTE SAT', data: '12' },
+      { title: 'TIPO CAMBIO SAT', data: '13' },
+      { title: 'AÑO DE FACTURACIÓN SAT', data: '14' },
+      { title: 'MES DE FACTURACIÓN SAT', data: '15' },
+      { title: 'TOTAL DE IMPUESTOS TRASLADADOS SAT', data: '16' },
+      { title: 'TOTAL DE IMPUESTOS RETENIDOS SAT', data: '17' },
+      { title: 'SERIE SAT', data: '18' },
+      { title: 'FOLIO SAT', data: '19' },
+      { title: 'USO DE CFDI SAT', data: '20' },
+      { title: 'FORMA DE PAGO SAT', data: '21' },
+      { title: 'MÉTODO DE PAGO SAT', data: '22' },
+      { title: 'MONEDA SAT', data: '23' },
+      { title: 'IVA TRASLADADO SAT', data: '24' },
+      { title: 'ISR TRASLADADO SAT', data: '25' },
+      { title: 'IEPS TRASLADADO SAT', data: '26' },
+      { title: 'IVA RETENIDO SAT', data: '27' },
+      { title: 'ISR RETENIDO SAT', data: '28' },
+      { title: 'IEPS RETENIDO SAT', data: '29' },
+      { title: 'SUBTOTAL SAT', data: '30' },
+      { title: 'PRIMER CONCEPTO SAT', data: '31' },
+      { title: 'CÓDIGO POSTAL EMISOR SAT', data: '32' },
+      { title: 'HORA SAT', data: '33' },
+      { title: 'VERSIÓN SAT', data: '34' },
+      { title: 'TOTAL IMPUESTOS LOCALES TRASLADADOS SAT', data: '35' },
+      { title: 'TOTAL IMPUESTOS LOCALES RETENIDOS SAT', data: '36' },
+      { title: 'DESCUENTO SAT', data: '37' },
+      { title: 'DOCUMENTOS RELACIONADOS SAT', data: '38' },
+      { title: 'TIPO DE RELACIÓN SAT', data: '39' },
+      { title: 'FOLIO FISCAL FACTO', data: '40' },
+      { title: 'FORMA DE PAGO FACTO', data: '41' },
+      { title: 'FECHA DE RECEPCIÓN FACTO', data: '42' },
+      { title: 'RFC EMISOR FACTO', data: '43' },
+      { title: 'CÓDIGO POSTAL EMISOR FACTO', data: '44' },
+      { title: 'RFC RECEPTOR FACTO', data: '45' },
+      { title: 'CÓDIGO POSTAL RECEPTOR FACTO', data: '46' },
+      { title: 'ESTATUS FACTO', data: '47' },
+      { title: 'TIPO DE DOCUMENTO FISCAL FACTO', data: '48' },
+      { title: 'SERIE FACTO', data: '49' },
+      { title: 'FOLIO FACTO', data: '50' },
+      { title: 'MONEDA FACTO', data: '51' },
+      { title: 'TIPO DE CAMBIO FACTO', data: '52' },
+      { title: 'IVA FACTO', data: '53' },
+      { title: 'IEPS FACTO', data: '54' },
+      { title: 'ISR FACTO', data: '55' },
+      { title: 'SUBTOTAL FACTO', data: '56' },
+      { title: 'DESCUENTO FACTO', data: '57' },
+      { title: 'TOTAL FACTO', data: '58' },
+      { title: 'FECHA FACTURA FACTO (YYYY-MM-DD)', data: '59' },
+      { title: 'FECHA FACTURA FACTO', data: '60' },
+      { title: 'FECHA TIMBRADO FACTO', data: '61' }
+    ];
+  }
+}
+
+export class ColumnsFaltantesValid {
+
+  columns_header: Array<{ title: string, data: string }>;
+
+  constructor() {
+    this.columns_header = [
+      { title: 'FOLIO FISCAL FACTO', data: '0' },
+      { title: 'FORMA DE PAGO FACTO', data: '1' },
+      { title: 'FECHA DE RECEPCIÓN FACTO', data: '2' },
+      { title: 'RFC EMISOR FACTO', data: '3' },
+      { title: 'CÓDIGO POSTAL EMISOR FACTO', data: '4' },
+      { title: 'RFC RECEPTOR FACTO', data: '5' },
+      { title: 'CÓDIGO POSTAL RECEPTOR FACTO', data: '6' },
+      { title: 'ESTATUS FACTO', data: '7' },
+      { title: 'TIPO DE DOCUMENTO FISCAL FACTO', data: '8' },
+      { title: 'SERIE FACTO', data: '9' },
+      { title: 'FOLIO FACTO', data: '10' },
+      { title: 'MONEDA FACTO', data: '11' },
+      { title: 'TIPO DE CAMBIO FACTO', data: '12' },
+      { title: 'IVA FACTO', data: '13' },
+      { title: 'IEPS FACTO', data: '14' },
+      { title: 'ISR FACTO', data: '15' },
+      { title: 'SUBTOTAL FACTO', data: '16' },
+      { title: 'DESCUENTO FACTO', data: '17' },
+      { title: 'TOTAL FACTO', data: '18' },
+      { title: 'FECHA FACTURA FACTO (YYYY-MM-DD)', data: '19' },
+      { title: 'FECHA FACTURA FACTO', data: '20' },
+      { title: 'FECHA TIMBRADO FACTO', data: '21' }
     ];
   }
 }
